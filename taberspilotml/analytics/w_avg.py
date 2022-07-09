2473b011256bc17e386e94e7063845c5f787ace7
CARLOS_DE_LA_CRUZ <tabers77@gmail.com>
2022-07-09 13:18:30 +0200

    weighted average updates

--- taberspilotml/analytics/w_avg.py
+++ taberspilotml/analytics/w_avg.py
@@ -1,72 +1,106 @@
 import pandas as pd
+import numpy as np
 
 
 def get_pct_per_column(df: pd.DataFrame, col: str):
-    df = df.copy()
-    tot = df[col].sum()
+    """
 
-    df[f'{col}_pct'] = df[col].apply(lambda x: x / tot * 100)
-    df.sort_values(f'{col}_pct', ascending=False, inplace=True)
-    df.reset_index(drop=True, inplace=True)
+    Args:
+        df:
+        col:
 
-    return df
+    Returns: pd.DataFrame
+
+    """
+    copy_df = df.copy()
+    total_sum = copy_df[col].sum()
+
+    copy_df[f'{col}_pct'] = copy_df[col].apply(lambda x: x / total_sum * 100)
+    copy_df.sort_values(f'{col}_pct', ascending=False, inplace=True)
+    copy_df.reset_index(drop=True, inplace=True)
+
+    return copy_df
 
 
-def get_weighted_avg_score(df: pd.DataFrame, weights: dict):
-    a = df.Clicks_pct * weights['w_clicks']
-    b = df.CTR_pct * weights['w_ctr']
-    c = (-df.Spent_pct + 1) * weights['w_spent']
-    d = df.importance_pct * weights['w_importance']
-    output = a + b + c + d
+def get_weighted_avg_score(df: pd.DataFrame, cols: list, weights: dict):
+    """
 
-    return round(output, 2)
+    Args:
+        cols: Ex: cols = ['Clicks', 'CTR', 'Spent', 'importance']
+        df:
+        weights: Ex: weights = {'w_clicks':0.15 , 'w_ctr': 0.15, 'w_spent': 0.25, 'w_importance': 0.45 }
+
+    Returns:
+
+    """
+    outputs = []
+    for col in cols:
+        output = df[f'{col}_pct'] * weights[f'weight_{col}']
+        outputs.append(output)
+
+    # a = df.Clicks_pct * weights['w_clicks']
+    # b = df.CTR_pct * weights['w_ctr']
+    # c = (-df.Spent_pct + 1) * weights['w_spent']   # negative weights
+    # d = df.importance_pct * weights['w_importance']
+    # output = a + b + c + d
+
+    # return round(output, 2)
 
 
-def compute_ranking(dataset: pd.DataFrame, weights: dict):
+def compute_ranking(dataset: pd.DataFrame, cols: list, weights: dict):
+    """
+
+    Args:
+        dataset:
+        cols: Ex: cols = ['Clicks', 'CTR', 'Spent', 'importance']
+        weights:
+
+    Returns:
+
+    """
     dataset = dataset.copy()
-    cols = ['Clicks', 'CTR', 'Spent', 'importance']
 
     for col in cols:
         dataset = get_pct_per_column(df=dataset, col=col)
 
-    dataset['Weighted Score'] = get_weighted_avg_score(df=dataset, weights=weights)
-    dataset['Ranking'] = dataset['Weighted Score'].rank(ascending=False)
+    dataset['weighted_score'] = get_weighted_avg_score(df=dataset, weights=weights)
+    dataset['ranking'] = dataset['weighted_score'].rank(ascending=False)
 
     return dataset
 
 
-def compute_final_ranking(dataset1: pd.DataFrame, dataset2: pd.DataFrame, by_name=False):
-    dataset1 = dataset1.copy()
-    dataset2 = dataset2.copy()
+def compute_final_ranking(df1: pd.DataFrame, df2: pd.DataFrame, by_name=False):
+    df1 = df1.copy()
+    df2 = df2.copy()
 
     if not by_name:
 
-        dataset1 = dataset1.groupby('Category').agg(
-            {'Weighted Score': 'mean', 'Clicks': 'mean', 'CTR': 'mean', 'importance': 'mean',
+        df1 = df1.groupby('Category').agg(
+            {'weighted_score': 'mean', 'Clicks': 'mean', 'CTR': 'mean', 'importance': 'mean',
              'Spent': 'mean'}).reset_index()
-        dataset2 = dataset2.groupby('Category').agg(
-            {'Weighted Score': 'mean', 'Clicks': 'mean', 'CTR': 'mean', 'importance': 'mean',
+        df2 = df2.groupby('Category').agg(
+            {'weighted_score': 'mean', 'Clicks': 'mean', 'CTR': 'mean', 'importance': 'mean',
              'Spent': 'mean'}).reset_index()
-        dataset1['Ranking'] = dataset1['Weighted Score'].rank(ascending=False)
-        dataset2['Ranking'] = dataset2['Weighted Score'].rank(ascending=False)
+        df1['ranking'] = df1['weighted_score'].rank(ascending=False)
+        df2['ranking'] = df2['weighted_score'].rank(ascending=False)
 
-        merged_df = pd.merge(dataset1, dataset2, on='Category')
+        merged_df = pd.merge(df1, df2, on='Category')
 
     else:
-        merged_df = pd.merge(dataset1.drop(index=4), dataset2.drop(index=4),
+        merged_df = pd.merge(df1.drop(index=4), df2.drop(index=4),
                              on='Name')  # Here we drop Facebook duplicates
 
     ranking_lst = []
 
     for i in range(len(merged_df)):
-        ranking_lst.append(np.median([merged_df.Ranking_x[i], merged_df.Ranking_y[i]]))
+        ranking_lst.append(np.median([merged_df.ranking_x[i], merged_df.ranking_y[i]]))
 
-    merged_df['Rank Score'] = ranking_lst
+    merged_df['rank_score'] = ranking_lst
 
-    merged_df = merged_df[['Category', 'Clicks_x', 'CTR_x', 'importance_x', 'Spent_x', 'Rank Score']].sort_values(
-        by='Rank Score').reset_index(drop=True) if not by_name \
-        else merged_df[['Name', 'Clicks_x', 'CTR_x', 'importance_x', 'Spent_x', 'Rank Score']].sort_values(
-        by='Rank Score').reset_index(drop=True)
+    merged_df = merged_df[['Category', 'Clicks_x', 'CTR_x', 'importance_x', 'Spent_x', 'rank_score']].sort_values(
+        by='rank_score').reset_index(drop=True) if not by_name \
+        else merged_df[['Name', 'Clicks_x', 'CTR_x', 'importance_x', 'Spent_x', 'rank_score']].sort_values(
+        by='rank_score').reset_index(drop=True)
 
     merged_df.rename(columns={'Clicks_x': 'Average Clicks', 'CTR_x': 'Average CTR',
                               'importance_x': 'Average Importance', 'Spent_x': 'Average Spent'},
